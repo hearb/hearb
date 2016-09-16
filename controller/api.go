f8dc1f955b5dbf5a9cd17567f6b641c9f6225077
himu62 <zero.256dic@gmail.com>
2016-09-16 14:45:40 +0900

    update

--- controller/api.go
+++ controller/api.go
@@ -1,12 +1,12 @@
 package controller
 
 import (
-	"github.com/hearb/hearb/api/v1"
+	"github.com/hearb/hearb/api"
 	mw "github.com/hearb/hearb/middleware"
 	"github.com/labstack/echo"
 )
 
 func AddAPIRoutes(e *echo.Echo) {
 	g := e.Group("/api/v1", mw.LoginRequire())
-	v1.AddUserRoutes(g)
+	api.AddUserRoutes(g)
 }
--- middleware/login_require.go
+++ middleware/login_require.go
@@ -1,12 +1,39 @@
 package middleware
 
 import (
+	"net/http"
+	"regexp"
+
+	"github.com/hearb/hearb/redis"
 	"github.com/labstack/echo"
 )
 
 func LoginRequire() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			// exclude routes
+			if matched, _ := regexp.MatchString(`^\/auth(\/.*)?$`, c.Path()); matched {
+				return next(c)
+			}
+
+			// check cookie
+			cookie, err := c.Cookie("session")
+			if err != nil || cookie.Value() == "" {
+				// cookie not found
+				return c.JSON(http.StatusUnauthorized, map[string]interface{}{"error": "login required"})
+			}
+
+			// get session from redis
+			client := redis.Client()
+			userID, err := client.Get(cookie.Value()).Int64()
+			if err != nil || userID == 0 {
+				// not logged in
+				return c.JSON(http.StatusUnauthorized, map[string]interface{}{"error": "login required"})
+			}
+
+			// store user ID onto context
+			c.Set("userID", uint32(userID))
+
 			return next(c)
 		}
 	}
